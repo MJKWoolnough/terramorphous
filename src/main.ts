3cb7ede0f19cccfa47364c545448f05975423b77
Michael Woolnough <michael.woolnough@gmail.com>
2023-03-10 12:58:13 +0000

    initial SPA creation and initialisation code

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import {clearNode} from './lib/dom.js';
+import ready from './lib/load.js';
+import {option, select, ul} from './lib/html.js';
+import {NodeMap, node} from './lib/nodes.js';
+import Clock from './clock.js';
+import {getZones} from './worldtime.js';
+
+type TimeZone = {
+	[node]: HTMLOptionElement;
+	zone: string;
+	clock?: Clock;
+}
+
+const defaultTimeZones = ["Africa/Cairo", "America/Los_Angeles", "America/New_York", "Asia/Hong_Kong", "Europe/London"],
+      fullList = new NodeMap<string, TimeZone>(select({"multiple": true, "size": 10})),
+      selectedList = new NodeMap<string, TimeZone>(select({"multiple": true, "size": 10})),
+      clockContainer = ul();
+
+ready
+.then(() => {
+	const local = new Clock("Local", 0);
+	
+	selectedList.set("Local", {
+		[node]: option("Local"),
+		zone: "Local",
+		clock: local
+	});
+	clockContainer.append(local.node);
+	fullList.set("", {
+		[node]: option({"disabled": true}, "Loading..."),
+		zone: "",
+	});
+	clearNode(document.body, [
+		clockContainer,
+		fullList[node],
+		selectedList[node]
+	]);
+})
+.then(getZones)
+.then(zones => {
+	fullList.delete("");
+	for (const zone of zones) {
+		if (defaultTimeZones.includes(zone)) {
+			selectedList.set(zone, {
+				[node]: option(zone),
+				zone,
+			});
+		} else {
+			fullList.set(zone, {
+				[node]: option(zone),
+				zone,
+			});
+		}
+	}
+
+})
+.catch(() => clearNode(document.body, "Failed to get Time Data"));
