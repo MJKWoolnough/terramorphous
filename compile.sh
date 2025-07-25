49d27fa7643e462cfe265a1586fa6e1d36b57268
Michael Woolnough <michael.woolnough@gmail.com>
2025-07-25 08:48:51 +0100

    re-formatted

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ cd "$bm";
 	cat icon.svg | tr -d '\n	' | sed -e 's/"/'"'"'/g' -e 's/#/\%23/g';
 	echo -n "\" />";
 	echo -n "<script type=\"module\">";
-	jspacker -n -i "/$(grep "<script" index.html | sed -e 's/.*src="\([^"]*\)".*/\1/')" | terser -m  --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n';
+	jspacker -n -i "/$(grep "<script" index.html | sed -e 's/.*src="\([^"]*\)".*/\1/')" | terser -m --module --compress pure_getters,passes=3 --ecma 6 | tr -d '\n';
 	echo -n "</script>";
 	tail -n4 index.html | tr -d '\n	';
 ) > "index.html";
